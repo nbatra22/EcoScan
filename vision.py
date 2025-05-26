274e9382971cdb411a96f396d40c13e508be2faf
Navya Batra <navya.batra@gmail.com>
2025-05-25 18:28:48 -0700

    vision api implemented labels extracted

---
+++ vision.py
@@ -0,0 +1,18 @@
+from google.cloud import vision
+import io
+
+client = vision.ImageAnnotatorClient.from_service_account_file('./gcp_key.json')
+
+image_path = './test.jpg'
+
+with io.open(image_path, 'rb') as image_file:
+  content = image_file.read()
+
+
+image = vision.Image(content=content)
+
+response = client.label_detection(image=image)
+labels = response.label_annotations
+
+for label in labels:
+  print(label.description, label.score)
