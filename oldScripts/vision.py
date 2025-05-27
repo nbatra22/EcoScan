1b226780c8b2c3ebd3d0b4a83160f3eed56ccebd
Navya Batra <navya.batra@gmail.com>
2025-05-26 17:31:40 -0700

    setting plantID and gps coordinates tg

--- oldScripts/vision.py
+++ oldScripts/vision.py
@@ -3,7 +3,7 @@ import io
 
 client = vision.ImageAnnotatorClient.from_service_account_file('./gcp_key.json')
 
-image_path = './test.jpg'
+image_path = './plant2.jpg'
 
 with io.open(image_path, 'rb') as image_file:
   content = image_file.read()
